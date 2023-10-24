c5fb6b781beeaefe70795f03ce04614228d7ebbc
Thay Mendes <thay.gm@live.com>
2023-10-24 12:41:47 -0300

    imc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+  const [imc, setIMC] = useState(null);
+  const [classificacao, setClassificacao] = useState('');
+
+  const calcularIMC = () => {
+    if (peso && altura) {
+      const pesoNumerico = parseFloat(peso.replace(',', '.'));
+      const alturaMetros = altura / 100;
+      const imcCalculado = (pesoNumerico / (alturaMetros * alturaMetros)).toFixed(2);
+      setIMC(imcCalculado);
+      classificarIMC(imcCalculado);
+    } else {
+      setIMC(null);
+      setClassificacao('');
+    }
+  };
+
+  const classificarIMC = (imc) => {
+    if (imc < 18.5) {
+      setClassificacao('Baixo Peso');
+    } else if (imc >= 18.5 && imc <= 24.9) {
+      setClassificacao('Peso Adequado');
+    } else if (imc >= 25 && imc <= 29.9) {
+      setClassificacao('Sobrepeso');
+    } else if (imc >= 30 && imc <= 34.9) {
+      setClassificacao('Obesidade Grau 1');
+    } else if (imc >= 35 && imc <= 39.9) {
+      setClassificacao('Obesidade Grau 2');
+    } else if (imc >= 40) {
+      setClassificacao('Obesidade Extrema');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Calculadora">
+      <h1>Calculadora de IMC</h1>
+      <div>
+        <label>Peso (kg):</label>
+        <input
+          type="text"
+          value={peso}
+          placeholder='Digite seu peso'
+          onChange={(e) => {
+            const value = e.target.value;
+            if (/^\d+(\.\d{0,2})?$/.test(value) || value === '') {
+              setPeso(value);
+            }
+          }}
+        />
+      </div>
+      <div>
+        <label>Altura (cm):</label>
+        <input
+          type="number"
+          value={altura}
+          placeholder='Digite sua altura em cm'
+          onChange={(e) => setAltura(e.target.value)}
+        />
+      </div>
+      <button onClick={calcularIMC}>Calcular IMC</button>
+      {imc && <p>Seu IMC é: {imc}</p>}
+      {classificacao && <p>Classificação: {classificacao}</p>}
     </div>
   );
 }
